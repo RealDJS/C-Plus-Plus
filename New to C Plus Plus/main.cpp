fd94358a1accd8d71a0a55f0e5cb84aa004d3d38
RealDJS <dj@skoys.com>
2024-10-04 17:44:34 -0600

    Finished making the classes and main program.

--- New to C Plus Plus/main.cpp
+++ New to C Plus Plus/main.cpp
@@ -1,22 +1,107 @@
 #include <iostream>
-using namespace std;
-
-
+#include <vector>
+#include <numbers>
+#include <iomanip>
 
 class GeometricObject {
+private:
+	int identifier;// A GeometricObject's 3 digit identifier.
+
 public:
-	void myMethod() {
-		cout << "Hello World!";
+	// Default Constructor
+	GeometricObject() : identifier(1) {};
+
+	// Parameterized Constructor
+	GeometricObject(int identifier) : identifier(identifier) {setIdentifier(identifier);}
+
+	// Virtual Destructor
+	virtual ~GeometricObject() = default;
+
+	// Getters & Setters
+	int getIdentifier() const { return identifier; }
+
+	void setIdentifier(int identifier) {
+		if (identifier >= 100 && identifier <= 999) {
+			this->identifier = identifier;
+		}
+		else if (identifier < 0) {
+			throw std::invalid_argument("Identifier cannot be negative.");
+		} else{
+			throw std::invalid_argument("Identifier must be a 3 - digit number.");
+		}
 	}
+
+	// Functions
+	virtual double calcArea() const = 0;
 };
 
 
-// Hello World
+class Square : public GeometricObject {
+	private: 
+		double side;// A square's side.
+	
+	public:
+		// Default Constructor
+		Square() : GeometricObject(), side(1) {}
+
+		// Parameterized Constructor
+		Square(int identifier, double sideLength) : GeometricObject(identifier), side(sideLength){}
+		
+		// Override calcArea function
+		double calcArea() const override{return side * side;};
+};
+
+
+class Circle : public GeometricObject{
+	private:
+		double radius; // A circle's radius.
+
+	public:
+		// Constructor
+		Circle() : GeometricObject(), radius(1) {}
+
+		// Parameterized Constructor
+		Circle(int identifier, double circleRadius): GeometricObject(identifier), radius(circleRadius){}
+
+		// Override calcArea function
+		double calcArea() const override {return radius * radius * std::numbers::pi; }
+};
+
+
+class RightTriangle : public GeometricObject {
+	private:
+		double height; // RightTriangle's height.
+		double base; // Right Triangle's base length.
+
+	public :
+		// Constructor
+		RightTriangle() : GeometricObject(), height(1), base(1) {}
+
+		// Parameterized Constructor
+		RightTriangle(int identifier, double triHeight, double triBase) : GeometricObject(identifier), height(triHeight), base(triBase) {}
+
+		// Override calcArea function
+		double calcArea() const override {return base * height / 2;}
+};
+
+
 int main()
 {
-	GeometricObject shape;
+	// Stores shapes polymorphically with smart pointers.
+	std::vector<std::shared_ptr<GeometricObject>> shapes;
 
-	shape.myMethod();
+	// Creates Shapes to store in the vector.
+	shapes.push_back(std::make_shared <Square>(100, 2));
+	shapes.push_back(std::make_shared <Circle>(134, 3.5));
+	shapes.push_back(std::make_shared<RightTriangle>(300, 4.5, 5.5));
+
+	// Set doubles to 2 decimals.
+	std::cout << std::fixed << std::setprecision(2);
+
+	// Display
+	for (const auto& shape : shapes) {
+		std::cout << "Shape ID: " << shape->getIdentifier() << ", Area: " << shape->calcArea() << std::endl;
+	}
 
 	return 0;
 }
--- sprint_time_log.md
+++ sprint_time_log.md
@@ -28,13 +28,15 @@ The expected minimum amount of time each Sprint for each category is as follows:
 |----------|----------|:------:|--------------------------------------------------------------|:-----------:|
 |9/23/24   | 16:15    |  IM    | Set up my dev environment for C++.                           |  75         |
 |9/24/24   | 15:45    |  IM    | Learned syntax, output, comments, & variables.               |  80         |
+|9/25/24   | 14:00    |  MTG   | Planning Meeting                                             |  60         |
 |9/25/24   | 18:25    |  IM    | Input, data & numeric types, boolean, Char, and string types.|  40         |
-|9/27/24   | 13:40    |  IM    | Operators                                                    |   60        |
-|9/27/24   | 17:55    |  IM    | Strings, Conditionals, Switch, loops, and Arrays             |   70        |
-|9/28/24   | 15:05    |  IM    | Pointers, Functions, Classes                                 |   90        |
-|9/30/24   | 14:00    |  TP    |                                                              |   60        |
-|9/30/24   | 18:45    |  IM    | Getting my Github repository to work correctly.              |   90        |
-|          |          |        |                                                              |             |
+|9/27/24   | 13:40    |  IM    | Operators                                                    |  60         |
+|9/27/24   | 17:55    |  IM    | Strings, Conditionals, Switch, loops, and Arrays             |  70         |
+|9/28/24   | 15:05    |  IM    | Pointers, Functions, Classes                                 |  90         |
+|9/30/24   | 14:00    |  MTG   | Project setup and planning.                                  |  60         |
+|9/30/24   | 18:45    |  IM    | Getting my Github repository to work correctly. Made a class.| 145         |
+|10/2/24   | 10:00    |  TP    | Presentation prep                                            |  60         |
+|10/2/24   | 14:00    |  MTG   | Team Review Meeting                                          |  60         |
 |          |          |        |                                                              |             |
 |          |          |        |                                                              |             |
 |          |          |        |                                                              |             |
